5f1437ebe1c1220832f44824d053a190e660811a
choiseona <tjsdk617@naver.com>
2023-12-20 21:18:11 +0900

    Chore: tailwind 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
