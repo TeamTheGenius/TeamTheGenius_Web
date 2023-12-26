662e7295c8e72df29840de433592265584250134
choiseona <tjsdk617@naver.com>
2023-12-27 00:40:25 +0900

    [#3] Feat: typography 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import "./index.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="font-_extraBold text-_h2">hello</h1>
+      <div className="font-_bold text-_h1">hello2</div>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,41 @@ export default {
   theme: {
     extend: {
       colors: {
-        my: {
-          primary: {
-            50: '#6054FF',
-            30: '#7579FF',
-            10: '#EDEEFF',
-          },
-          accent: {
-            30: '#FF6584',
-          },
-          neutral: {
-            70: '#3F3D56',
-            80: '#2F2E41',
-            10: '#E6E6E6',
-            50: '#858587',
-          },
-        }
+        _primary: {
+          50: '#6054FF',
+          30: '#7579FF',
+          10: '#EDEEFF',
+        },
+        _accent: {
+          30: '#FF6584',
+        },
+        _neutral: {
+          70: '#3F3D56',
+          80: '#2F2E41',
+          10: '#E6E6E6',
+          50: '#858587',
+        },
 
+      },
+      fontFamily: {
+        _black: 'PretendardBlack',
+        _bold: 'PretendardBold',
+        _extraBold: 'PretendardExtraBold',
+        _extraLight: 'PretendardExtraLight',
+        _light: 'PretendardLight',
+        _medium: 'PretendardMedium',
+        _regular: 'PretendardRegular',
+        _semiBold: 'PretendardSemiBold',
+        _thin: 'PretendardThin',
+      },
 
+      fontSize: {
+        _h1: "3.2rem",
+        _h2: "2.4rem",
+        _h3: "2rem",
+        _h4: "1.8rem",
+        _body1: "1.6rem",
+        _caption: "1.4rem",
       }
     },
   },
