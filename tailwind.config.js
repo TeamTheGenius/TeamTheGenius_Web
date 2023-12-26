f76a74349ffba7371084bbf30116d413c44f6b6b
choiseona <tjsdk617@naver.com>
2023-12-26 23:19:43 +0900

    [#3] Feat: color 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        my: {
+          primary: {
+            50: '#6054FF',
+            30: '#7579FF',
+            10: '#EDEEFF',
+          },
+          accent: {
+            30: '#FF6584',
+          },
+          neutral: {
+            70: '#3F3D56',
+            80: '#2F2E41',
+            10: '#E6E6E6',
+            50: '#858587',
+          },
+        }
+
+
+      }
+    },
   },
   plugins: [],
 }
