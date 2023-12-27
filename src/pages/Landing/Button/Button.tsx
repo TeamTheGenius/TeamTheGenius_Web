158789aeb943a8ae9df73f3a1f8ea178556240cb
choiseona <tjsdk617@naver.com>
2023-12-27 23:17:54 +0900

    Feat: Landing 페이지 컴포넌트 조합, 완성
    Related to: #11

---
+++ src/pages/Landing/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../../components/Button";
+
+function Landing() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
+  return (
+    <Button
+      content={"시작하기"}
+      width={"w-full"}
+      height={"h-[61px]"}
+      backgroundColor={"bg-_primary-50"}
+      textSize={"text-_h2"}
+      textColor={"text-white"}
+      fontWeight={"font-semibold"}
+      handleClick={handleClick}
+    />
+  );
+}
+
+export default Landing;
--- src/pages/Landing/Image/Image.tsx
+++ src/pages/Landing/Image/Image.tsx
@@ -1,9 +1,7 @@
 import landingImage from "../../../assets/image/landing-image.png";
 
 function Image() {
-  return (
-    <img src={landingImage} alt="landing page image" className="w-[436px]" />
-  );
+  return <img src={landingImage} alt="landing page image" className="w-full" />;
 }
 
 export default Image;
