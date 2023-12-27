529f4d1375902995942d95233cb43366f13036bc
choiseona <tjsdk617@naver.com>
2023-12-27 21:44:27 +0900

    Feat: Landing페이지의 Image 컴포넌트 ui
    Related to: #11

---
+++ src/pages/Landing/Image/Image.tsx
@@ -0,0 +1,9 @@
+import landingImage from "../../../assets/image/landing-image.png";
+
+function Image() {
+  return (
+    <img src={landingImage} alt="landing page image" className="w-[436px]" />
+  );
+}
+
+export default Image;
