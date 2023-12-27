59a108ab5957d74b1cd7cefcd26e1551a171d3c8
choiseona <tjsdk617@naver.com>
2023-12-27 23:37:22 +0900

    Fix: 이상한 여백 생기는 문제 해결
    Related to: #11

--- src/pages/Landing/Image/Image.tsx
+++ src/pages/Landing/Image/Image.tsx
@@ -1,7 +1,13 @@
 import landingImage from "../../../assets/image/landing-image.png";
 
 function Image() {
-  return <img src={landingImage} alt="landing page image" className="w-full" />;
+  return (
+    <img
+      src={landingImage}
+      alt="landing page image"
+      className="w-[472px] px-[18px]"
+    />
+  );
 }
 
 export default Image;
