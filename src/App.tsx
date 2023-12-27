cd8f12dbe22561d467e5af8772ed4f36ae1ceebc
choiseona <tjsdk617@naver.com>
2023-12-27 14:26:12 +0900

    Feat: 로그인, 회원가입 라우터 설정
    Related to: #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/singUp/SignUp";
+import Landing from "./pages/Landing/Landing";
+import LogIn from "./pages/LogIn/LogIn";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/login/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/LogIn/LogIn.tsx
@@ -0,0 +1,5 @@
+function LogIn() {
+  return <div></div>;
+}
+
+export default LogIn;
