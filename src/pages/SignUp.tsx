e3866be94b9de5295441d8707a61481abc6d81bb
songmok <songmokseed@gmail.com>
2023-12-27 09:24:40 +0900

    Feat: 회원가입 페이지 작성 중
    - 관심사 항목은 더 좋은 방법을 찾기 위해 비워뒀습니다.
    - 라이브러리를 추가했습니다.(formik, react-route-dom, yup)
    
    Related to: [#6]

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,95 @@
+import MobView from "../components/MobView";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const SignUp = () => {
+  const validationSchema = Yup.object().shape({
+    nickName: Yup.string()
+      .required("닉네임을 입력해주세요.")
+      .min(2, "닉네임은 최소 2글자 이상이어야 합니다.")
+      .max(15, "닉네임은 최대 15글자까지 입력 가능합니다."),
+    myInfo: Yup.string().max(9, "자기소개는 최대 150자까지 입력 가능합니다"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      nickName: "",
+      myInfo: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div>
+      <MobView>
+        <div className="flex">
+          <h1 className="pretendard font-bold font-head flex items-end">
+            투두핀에
+            <br />
+            오신 것을 환영합니다
+          </h1>
+          <span className="pretendard text-sm ml-auto">
+            <b className="nomal mr-1">*</b>는 필수 입력란입니다.
+          </span>
+        </div>
+        <form onSubmit={formik.handleSubmit}>
+          <ul className="py-6">
+            <li className="mb50 flex flex-col">
+              <label htmlFor="nickName" className="signUp-lable required">
+                프로필 이름
+              </label>
+              <input
+                className={`pretendard signUp-placeholder signUp-input`}
+                type="text"
+                id="nickName"
+                name="nickName"
+                placeholder="2 ~ 15까지 입력해주세요."
+                minLength={2}
+                maxLength={15}
+                value={formik.values.nickName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.nickName && formik.errors.nickName ? (
+                <div className="pretendard singUp-err">
+                  {formik.errors.nickName}
+                </div>
+              ) : null}
+            </li>
+            <li className="mb50 flex flex-col">
+              <label htmlFor="myInfo" className="signUp-lable">
+                자기소개
+              </label>
+              <input
+                className={`pretendard signUp-placeholder signUp-input`}
+                type="text"
+                id="myInfo"
+                name="myInfo"
+                placeholder="한 줄로 투두메이트들에게 자신을 소개해주세요. (150자까지)"
+                maxLength={150}
+                value={formik.values.myInfo}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.myInfo && formik.errors.myInfo ? (
+                <div className="pretendard singUp-err">
+                  {formik.errors.myInfo}
+                </div>
+              ) : null}
+            </li>
+          </ul>
+          <button className="btn1">
+            <span className="text-white flex justify-center items-center h-full">
+              가입하기
+            </span>
+          </button>
+        </form>
+      </MobView>
+    </div>
+  );
+};
+
+export default SignUp;
