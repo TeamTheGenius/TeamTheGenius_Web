450ad1896093198a18364617eabe93ad43ed7d13
choiseona <tjsdk617@naver.com>
2023-12-28 16:49:20 +0900

    Feat: 로그인 Buttons 컴포넌트 완성
    Related to: #21

--- src/pages/LogIn/Buttons/Buttons.tsx
+++ src/pages/LogIn/Buttons/Buttons.tsx
@@ -5,12 +5,12 @@ import NaverButton from "../NaverButton/NaverButton";
 
 function Buttons() {
   return (
-    <>
+    <div className="flex flex-col gap-[1.3rem] p-[8px]">
       <KakaoButton />
       <NaverButton />
       <FacebookButton />
       <GoogleButton />
-    </>
+    </div>
   );
 }
 
