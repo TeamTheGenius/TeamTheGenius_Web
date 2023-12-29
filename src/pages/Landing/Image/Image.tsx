871ad9c42872652865531e2d59dfdc9a532a81de
choiseona <tjsdk617@naver.com>
2023-12-29 22:57:54 +0900

    Feat: Landing 페이지 이미지 최적화
    Related to: #43

--- src/pages/Landing/Image/Image.tsx
+++ src/pages/Landing/Image/Image.tsx
@@ -1,13 +1,21 @@
-import landingImage from "@/assets/image/landing-image.png";
+import landingImageWebp from "@/assets/image/landing-image.webp";
+import landingImagePng from "@/assets/image/landing-image.png";
 
 function Image() {
   return (
     <div className="relative w-full pb-[119.5%]">
-      <img
-        src={landingImage}
-        alt="landing page image"
-        className="w-full h-full absolute top-0 left-0"
-      />
+      <picture>
+        <source
+          srcSet={landingImageWebp}
+          type="image/webp"
+          className="w-full h-full absolute top-0 left-0"
+        />
+        <img
+          src={landingImagePng}
+          alt="landing page image"
+          className="w-full h-full absolute top-0 left-0"
+        />
+      </picture>
     </div>
   );
 }
