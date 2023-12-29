1406fa3ef9d8e913914be60338e8057045162595
choiseona <tjsdk617@naver.com>
2023-12-30 00:04:37 +0900

    Feat: LogIn 페이지 이미지 최적화
    Related to: #43

--- src/pages/LogIn/Image/Image.tsx
+++ src/pages/LogIn/Image/Image.tsx
@@ -1,12 +1,20 @@
-import heroImage from "@/assets/image/Hero.jpg";
+import heroImageJpg from "@/assets/image/Hero.jpg";
+import heroImageWebp from "@/assets/image/hero.webp";
 function Image() {
   return (
     <div className="w-full relative pb-[96.04%]">
-      <img
-        src={heroImage}
-        alt="login page comment"
-        className="w-full h-full absolute top-0 left-0"
-      />
+      <picture>
+        <source
+          srcSet={heroImageWebp}
+          type="image/webp"
+          className="w-full h-full absolute top-0 left-0"
+        />
+        <img
+          src={heroImageJpg}
+          alt="login page comment"
+          className="w-full h-full absolute top-0 left-0"
+        />
+      </picture>
     </div>
   );
 }
