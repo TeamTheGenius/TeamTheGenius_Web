d0b119796f5887b07488c659d437b8bd07d68e53
choiseona <tjsdk617@naver.com>
2024-01-03 22:43:05 +0900

    Feat: friends-list 슬라이드
    Related to: #64

--- src/components/Main/FriendsInOnline/FriendsInOnline.tsx
+++ src/components/Main/FriendsInOnline/FriendsInOnline.tsx
@@ -5,14 +5,14 @@ import friendsInOnlineData from "./friendsInOnline.json";
 
 function FriendsInOnline() {
   return (
-    <div className="flex justify-center items-center gap-[2.3rem]">
+    <div className="flex justify-center items-center gap-[2.3rem] w-full">
       <UserCircle>
         <UserCircle.UserImage userImage={test} />
         <UserCircle.UserName userName="me" />
       </UserCircle>
 
       <div
-        className="w-[3rem] h-[3rem] rounded-full flex justify-center items-center"
+        className="w-[3rem] h-[3rem] rounded-full flex justify-center items-center shrink-0"
         style={{
           background: "linear-gradient(180deg, #E5EBF0 0%, #E6F0F9 100%)",
         }}
@@ -20,7 +20,7 @@ function FriendsInOnline() {
         <img src={toggleButton} alt="friends-list toggle button" />
       </div>
 
-      <div className="flex gap-[1.7rem]">
+      <div className="flex gap-[1.7rem] overflow-scroll scrollbar-hide">
         {friendsInOnlineData.friendsInOnlineData.map((friend, index) => (
           <UserCircle key={`${index} ${friend.name}`}>
             <UserCircle.UserImage userImage={friend.image}>
