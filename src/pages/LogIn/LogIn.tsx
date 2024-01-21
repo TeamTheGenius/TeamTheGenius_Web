cd4ba8634379a2cb27002df85b9a06b0ffa52835
choiseona <tjsdk617@naver.com>
2024-01-21 16:31:47 +0900

    Remove: 필요없는 파일 삭제
    Related to: #80

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,7 +1,6 @@
 import MobCard from "@/components/Common/MobCard";
 import Comment from "@/components/LogIn/Comment/Comment";
 import GithubLoginButton from "@/components/LogIn/GithubLoginButton";
-import Image from "@/components/LogIn/Image/Image";
 import TermsOfService from "@/components/LogIn/TermsOfService/TermsOfService";
 
 function LogIn() {
@@ -9,9 +8,6 @@ function LogIn() {
     <MobCard>
       <div className="w-full flex flex-col gap-[5.6rem] justify-center items-center">
         <Comment />
-        <div className="w-full max-w-[202px]">
-          <Image />
-        </div>
         <GithubLoginButton />
       </div>
       <div className="mb-[2.1rem]" />
