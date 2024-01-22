2f682cb90d7a57c1ea08863f1bb963caa434993c
choiseona <tjsdk617@naver.com>
2024-01-22 12:32:07 +0900

    Feat: ChallengeItem UI 부분 완성
    Related to: #83

--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -1,12 +1,52 @@
+import ChallengeItem from "../ChallengeItem/ChallengeItem";
 import MoreButton from "../MoreButton/MoreButton";
 import Title from "../Title/Title";
+import { popularChallengeData } from "@/data/pupularChallengeData";
+import { suggestionChallengeData } from "@/data/suggestionChallengeData";
 
 function Home() {
   return (
-    <div className="flex gap-[2.851rem] items-center">
-      <Title content="추천 챌린지" />
-      <MoreButton keyUrl="suggestion" />
-    </div>
+    <>
+      <div className="flex gap-[2.851rem] justify-between items-center">
+        <Title content="추천 챌린지" />
+        <MoreButton keyUrl="suggestion" />
+      </div>
+
+      <div className="overflow-auto">
+        <div className="flex gap-[2.2rem] w-screen">
+          {suggestionChallengeData.map((item, index) => (
+            <ChallengeItem key={index}>
+              <ChallengeItem.Image imgSrc={item.imgSrc} alt={item.alt}>
+                <ChallengeItem.NumberOfParticipant
+                  numberOfParticipants={item.numberOfParticipants}
+                />
+              </ChallengeItem.Image>
+              <ChallengeItem.Title title={item.title} />
+            </ChallengeItem>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex gap-[2.851rem] justify-between items-center">
+        <Title content="인기 챌린지" />
+        <MoreButton keyUrl="popular" />
+      </div>
+
+      <div className="overflow-auto">
+        <div className="flex gap-[2.2rem] w-screen">
+          {popularChallengeData.map((item, index) => (
+            <ChallengeItem key={index}>
+              <ChallengeItem.Image imgSrc={item.imgSrc} alt={item.alt}>
+                <ChallengeItem.NumberOfParticipant
+                  numberOfParticipants={item.numberOfParticipants}
+                />
+              </ChallengeItem.Image>
+              <ChallengeItem.Title title={item.title} />
+            </ChallengeItem>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/data/pupularChallengeData.ts
@@ -0,0 +1,10 @@
+import test from "@/assets/image/landing-image.png";
+
+export const popularChallengeData = [
+  { imgSrc: test, alt: "test", numberOfParticipants: 25, title: "1일 1커밋" },
+  { imgSrc: test, alt: "test", numberOfParticipants: 26, title: "1일 2커밋" },
+  { imgSrc: test, alt: "test", numberOfParticipants: 27, title: "1일 3커밋" },
+  { imgSrc: test, alt: "test", numberOfParticipants: 28, title: "1일 4커밋" },
+  { imgSrc: test, alt: "test", numberOfParticipants: 29, title: "1일 5커밋" },
+  { imgSrc: test, alt: "test", numberOfParticipants: 25, title: "1일 1커밋" },
+];
