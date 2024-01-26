1159fda2046e26942472b7a72ba2e2e36c149ef8
songmok <songmokseed@gmail.com>
2024-01-27 06:32:52 +0900

    Feat: 하단 메뉴 완성 하단 메뉴 아이콘 추가 Resolves: #90 #94

--- src/components/common/BottomNav/BottomNav.tsx
+++ src/components/common/BottomNav/BottomNav.tsx
@@ -1,30 +1,61 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "@/components/Common/BottomNav/BottomNavStyle.css";
-const navData = [
-  { to: "/main/home", title: "홈" },
-  { to: "/main/my-challenge", title: "마이챌린지" },
-  { to: "/main/pet", title: "펫" },
-  { to: "/main/my-page", title: "마이페이지" },
-];
+import icon_off_1 from "@/assets/icon/icon_off_1.png";
+import icon_off_2 from "@/assets/icon/icon_off_2.png";
+import icon_off_3 from "@/assets/icon/icon_off_3.png";
+import icon_off_4 from "@/assets/icon/icon_off_4.png";
+import icon_on_1 from "@/assets/icon/icon_on_1.png";
+import icon_on_2 from "@/assets/icon/icon_on_2.png";
+import icon_on_3 from "@/assets/icon/icon_on_3.png";
+import icon_on_4 from "@/assets/icon/icon_on_4.png";
+
 function BottomNav() {
+  const navData = [
+    { to: "/main/home", title: "홈" },
+    { to: "/main/mychallenge", title: "마이챌린지" },
+    { to: "/main/pet", title: "펫" },
+    { to: "/main/mypage", title: "마이페이지" },
+  ];
+  const iconOff = [icon_off_1, icon_off_2, icon_off_3, icon_off_4];
+  const iconOn = [icon_on_1, icon_on_2, icon_on_3, icon_on_4];
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNavLinkClick = (index: number) => {
+    setActiveIndex(index);
+  };
+  useEffect(() => {
+    setImageLoaded(true);
+  }, []);
+
   return (
     <>
       <ul className="fixed w-full bottom-0 bg-white shadow-md flex items-center justify-around max-w-[773px] mx-auto">
         {navData.map((data, i) => {
+          const offIcon = iconOff[i];
+          const onIcon = iconOn[i];
+          const isActive = activeIndex === i;
           return (
             <li className="w-full" key={i}>
-              <NavLink
-                to={data.to}
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? "nav-pending"
-                    : isActive
-                    ? "nav-active"
-                    : "nav-pending"
-                }
-              >
-                <span>{data.title}</span>
-              </NavLink>
+              {!imageLoaded ? (
+                <></>
+              ) : (
+                <>
+                  <NavLink
+                    to={data.to}
+                    className={isActive ? "nav-active" : "nav-pending"}
+                    onClick={() => handleNavLinkClick(i)}
+                  >
+                    <img
+                      src={isActive ? onIcon : offIcon}
+                      className={isActive ? "loaded" : "pending"}
+                      alt={`아이콘 off ${i + 1}`}
+                    />
+                    <span>{data.title}</span>
+                  </NavLink>
+                </>
+              )}
             </li>
           );
         })}
