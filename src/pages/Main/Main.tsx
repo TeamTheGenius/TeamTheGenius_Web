4e8c22ce4d01604581de3fde93d0ef0aa531a236
choiseona <tjsdk617@naver.com>
2024-01-31 01:11:22 +0900

    Feat: 검색 로직 수정
    Related to: #113

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,19 @@
 import BottomNav from "@/components/Common/BottomNav/BottomNav";
 import MobCard from "@/components/Common/MobCard";
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { PATH } from "@/constants/path";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 function Main() {
+  const { pathname } = useLocation();
   const [searchQuery, setSearchQuery] = useState<string>("");
+
+  useEffect(() => {
+    if (pathname !== PATH.SEARCH) {
+      setSearchQuery("");
+    }
+  }, [pathname]);
+
   return (
     <MobCard>
       <div className="pb-[9rem] _sm:pb-[6.1rem]">
