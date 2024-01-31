257a50237c7e352772431de6f632d72496ef9f7b
choiseona <tjsdk617@naver.com>
2024-02-01 04:12:55 +0900

    Fix: css 수정
    Related to: #119

--- src/components/common/MobCard.tsx
+++ src/components/common/MobCard.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const MobCard = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center">
       <div className="w-[773px] min-h-[100vh] bg-white flex justify-center">
         <div className="w-full">{children}</div>
       </div>
