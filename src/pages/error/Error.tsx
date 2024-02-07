d4f2689b3e681571295ef8107093ef67fcba3225
songmok <118712217+songmok@users.noreply.github.com>
2024-02-07 10:21:40 +0900

    feat: loading-error UI 완성
    Resolves: #129

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,26 +1,38 @@
 import ErrorHeader from "@/components/Error/ErrorHeader/ErrorHeader";
 import Button from "@/components/Common/Button";
 import MobCard from "@/components/Common/MobCard";
+import "@/pages/Error/errorStyle.css";
 import { useNavigate } from "react-router-dom";
+export type ErrorHeaderType = {
+  errNum?: number;
+  errorTxt?: string;
+};
 
-const Error = () => {
+const Error = ({ errNum, errorTxt }: ErrorHeaderType) => {
   const navigate = useNavigate();
+
   const back = () => {
     navigate(-1);
   };
   return (
     <MobCard>
-      <ErrorHeader />
-      <Button
-        content={"뒤로 가기"}
-        width={"w-full"}
-        height={"h-[6.1rem]"}
-        backgroundColor={"bg-_primary-50"}
-        textSize={"text-_h2"}
-        textColor={"text-white"}
-        fontWeight={"font-semibold"}
-        handleClick={back}
-      />
+      <div className="relative w-full h-[100vh]">
+        <ErrorHeader errNum={errNum} errorTxt={errorTxt} />
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 box-shodow">
+          <div className="mx-[153px] my-[19px]">
+            <Button
+              content={"뒤로 가기"}
+              width={"w-[467px]"}
+              height={"h-[6.1rem]"}
+              backgroundColor={"bg-black"}
+              textSize={"text-[1.8rem]"}
+              textColor={"text-white"}
+              fontWeight={"font-medium"}
+              handleClick={back}
+            />
+          </div>
+        </div>
+      </div>
     </MobCard>
   );
 };
