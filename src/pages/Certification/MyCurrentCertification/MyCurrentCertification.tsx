f1c8f54057815165569fbeb3af88c29cbaabe7e0
choiseona <tjsdk617@naver.com>
2024-02-15 00:31:15 +0900

    Feat: MyAllCertificationLinkButton과 CurrentAchivementRate 컴포넌트 완성
    Related to: #149

--- src/pages/Certification/MyCurrentCertification/MyCurrentCertification.tsx
+++ src/pages/Certification/MyCurrentCertification/MyCurrentCertification.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import CurrentAchivementRate from "@/components/Certification/MyCurrentCertification/CurrentAchivementRate/CurrentAchivementRate";
+import MyAllCertificationLinkButton from "@/components/Certification/MyCurrentCertification/MyAllCertificationLinkButton/MyAllCertificationLinkButton";
 
 function MyCurrentCertification() {
-  return <div>myCurrentCertification</div>;
+  return (
+    <div className="px-[2.2rem]">
+      <div className="mt-[2.4rem] _sm:mt-[2rem]">
+        <MyAllCertificationLinkButton />
+      </div>
+      <div className=" mt-[1.4rem] ">
+        <CurrentAchivementRate />
+      </div>
+    </div>
+  );
 }
 
 export default MyCurrentCertification;
