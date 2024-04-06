6d78c7b5d542f875adb0773a59430832245b4cef
choiseona <tjsdk617@naver.com>
2024-04-06 19:44:01 +0900

    Refactor: postProfileInterest customHook으로 분리
    Related to: #321

--- src/hooks/queries/useProfileQuery.ts
+++ src/hooks/queries/useProfileQuery.ts
@@ -1,6 +1,7 @@
 import deleteServiceWithdraw from "@/apis/deleteServiceWithdraw";
 import getInterestTags from "@/apis/getInterestTags";
 import getMyPageProfile from "@/apis/getMyPageProfile";
+import postInterestEditApi from "@/apis/postInterestEditApi";
 import postUserProfile from "@/apis/postUserProfile";
 import { FRAMEID, IDENTIFIER } from "@/constants/localStorageKey";
 import { PATH } from "@/constants/path";
@@ -66,11 +67,13 @@ export const useDeleteUser = () => {
   return { mutate };
 };
 
-interface Params {
+interface useGetMyProfileInterestTagParams {
   onSuccess: (check: CheckboxValueType[]) => void;
 }
 
-export const useGetMyProfileInterestTag = ({ onSuccess }: Params) => {
+export const useGetMyProfileInterestTag = ({
+  onSuccess,
+}: useGetMyProfileInterestTagParams) => {
   const { data } = useQuery<string[]>({
     queryKey: [QUERY_KEY.MY_INTEREST_TAGS],
     queryFn: () => getInterestTags(),
@@ -78,4 +81,25 @@ export const useGetMyProfileInterestTag = ({ onSuccess }: Params) => {
   });
 
   return { data };
+};
+
+interface usePostMyProfileInterestTagParams {
+  onSuccess: () => void;
+  onError: () => void;
+}
+
+export const usePostMyProfileInterestTag = ({
+  onSuccess,
+  onError,
+}: usePostMyProfileInterestTagParams) => {
+  const { mutate } = useMutation(
+    (checkedValues: CheckboxValueType[]) =>
+      postInterestEditApi({ interestEditData: checkedValues }),
+    {
+      onSuccess: onSuccess,
+      onError: onError,
+    }
+  );
+
+  return { mutate };
 };
