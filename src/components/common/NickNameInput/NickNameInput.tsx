ed0ea5262f956c1e6a501e3b161e1e5edc8b3f87
choiseona <tjsdk617@naver.com>
2024-04-08 21:13:40 +0900

    Refactor: getCheckNickName customHook으로 분리
    Related to: #321

--- src/components/common/NickNameInput/NickNameInput.tsx
+++ src/components/common/NickNameInput/NickNameInput.tsx
@@ -1,5 +1,4 @@
 import Button from "@/components/Common/Button";
-
 import React, {
   ChangeEvent,
   Dispatch,
@@ -8,9 +7,10 @@ import React, {
   useState,
 } from "react";
 import nickname_X from "@/assets/icon/nickname_X.svg";
-import { getCheckNicknameApi } from "@/apis/getCheckNicknameApi";
 import Loading from "../Loading/Loading";
 import { EditModal } from "@/components/MyPage/EditModal/EditModal";
+import { useGetCheckNickName } from "@/hooks/queries/useUserQuery";
+import { AxiosError } from "axios";
 
 type SignUpInputProps = {
   label: string;
@@ -59,6 +59,22 @@ const NickNameInput: React.FC<SignUpInputProps> = ({
 }) => {
   const [isLoading, setIsLoading] = useState(false);
 
+  const onSuccessGetCheckNickName = () => {
+    setIsLoading(false);
+    setsignUpBoolean(true);
+    setNickCheck("사용 가능한 닉네임입니다.");
+  };
+  const onErrorGetCheckNickName = (err: AxiosError) => {
+    setIsLoading(false);
+    setsignUpBoolean(false);
+    setNickCheck(err?.response?.data?.message);
+  };
+  const { mutate: getCheckNinkNameMutate } = useGetCheckNickName({
+    onSuccess: onSuccessGetCheckNickName,
+    onError: onErrorGetCheckNickName,
+  });
+  const specialChars = /[!@#$%^&*(),.?":{}|<>]/;
+
   const sameNickCheck = () => {
     openModal();
     setModal(
@@ -73,13 +89,12 @@ const NickNameInput: React.FC<SignUpInputProps> = ({
     );
   };
   const nickNameCheck = () => {
-    setIsLoading(true);
-    getCheckNicknameApi({
-      value,
-      setNickCheck,
-      setsignUpBoolean,
-      setIsLoading,
-    });
+    if (specialChars.test(value)) {
+      alert("특수문자는 사용할 수 없습니다.");
+    } else {
+      setIsLoading(true);
+      getCheckNinkNameMutate({ value });
+    }
   };
 
   const resetValue = () => {
--- src/hooks/queries/useUserQuery.ts
+++ src/hooks/queries/useUserQuery.ts
@@ -1,11 +1,12 @@
 import signUpApi from "@/apis/postSignUpApi";
 import { IDENTIFIER } from "@/constants/localStorageKey";
 import { CheckboxValueType } from "antd/es/checkbox/Group";
-import { AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import { useMutation } from "react-query";
 import { encrypt } from "../useCrypto";
 import { useNavigate } from "react-router-dom";
 import { PATH } from "@/constants/path";
+import { getCheckNicknameApi } from "@/apis/getCheckNicknameApi";
 
 interface PostSignUpMutateType {
   identifier: string;
@@ -39,6 +40,32 @@ export const usePostSignUp = ({ onSuccess, onError }: PostSignUpType) => {
       onError: () => {
         navigate(PATH.LOGIN);
         onError();
+      },
+      useErrorBoundary: false,
+    }
+  );
+  return { mutate };
+};
+
+interface GetCheckNinkNameMutateType {
+  value: string;
+}
+interface GetCheckNinkNameType {
+  onSuccess: () => void;
+  onError: (err: AxiosError) => void;
+}
+export const useGetCheckNickName = ({
+  onSuccess,
+  onError,
+}: GetCheckNinkNameType) => {
+  const { mutate } = useMutation(
+    ({ value }: GetCheckNinkNameMutateType) => getCheckNicknameApi({ value }),
+    {
+      onSuccess: () => {
+        onSuccess();
+      },
+      onError: (err: AxiosError) => {
+        onError(err);
       },
       useErrorBoundary: false,
     }
