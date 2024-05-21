69214c0a42d7b064ded9cb825e9671fdf36bfa70
choiseona <tjsdk617@naver.com>
2024-05-21 20:28:13 +0900

    modal store 생성
    Related to: #369

--- src/apis/postPaymentSuccess.ts
+++ src/apis/postPaymentSuccess.ts
@@ -21,7 +21,7 @@ const postPaymentSuccess = async ({
   };
 
   await instance
-    .post("https://api.gitget.co.kr//api/payment/toss/success", body, {
+    .post("http://localhost:8080/api/payment/toss/success", body, {
       headers: {
         Authorization: encryptedSecretKey,
       },
