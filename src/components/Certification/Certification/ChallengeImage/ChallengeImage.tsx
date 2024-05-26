684d7da38aa803d5d9cf807df61c80de2d491d3d
choiseona <tjsdk617@naver.com>
2024-05-26 16:20:31 +0900

    Feat: 챌린지 이미지 대체 이미지 설정
    Related to: #380

--- src/components/Certification/Certification/ChallengeImage/ChallengeImage.tsx
+++ src/components/Certification/Certification/ChallengeImage/ChallengeImage.tsx
@@ -1,14 +1,23 @@
+import basicChallengeImage from "@/assets/image/basicChallengeImage.jpg";
+
 interface Props {
   imgSrc: string;
   alt: string;
 }
 
 function ChallengeImage({ imgSrc, alt }: Props) {
+  const onErrorImageLoad = (
+    event: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    event.currentTarget.src = basicChallengeImage;
+  };
+
   return (
     <div className="w-full h-full pb-[55%] relative">
       <img
         src={imgSrc}
         alt={alt}
+        onError={onErrorImageLoad}
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
       <div className="absolute top-0 left-0 w-full h-full opacity-35 bg-black " />
