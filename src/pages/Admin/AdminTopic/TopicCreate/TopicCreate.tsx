8969617490e52d0bcc22b58bf8aa0c575ca61e1f
choiseona <tjsdk617@naver.com>
2024-12-23 18:47:00 +0900

    Feat: TopicCreate의 Button, Form, Input, Select를 antd 없이 구현

--- src/pages/Admin/AdminTopic/TopicCreate/TopicCreate.tsx
+++ src/pages/Admin/AdminTopic/TopicCreate/TopicCreate.tsx
@@ -1,33 +1,25 @@
 import AdminFormLayOut from "@/components/Admin/AdminLayOut/AdminFormLayOut/AdminFormLayOut";
+import { Input, Select, TextArea } from "@/components/Common/Form";
 import LoadingBox from "@/components/Common/Loading/LoadingBox/LoadingBox";
 import { interestsOption } from "@/data/InterestData";
 import { usePostTopicCreate } from "@/hooks/queries/useAdminTopicQuery";
-import { Button, Form, Input, Select } from "antd";
-import { useRef, useState } from "react";
+import { encrypt } from "@/hooks/useCrypto";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 type FormDataType = {
   description: string;
   notice: string;
-  pointPerPerson: string;
-  tags: string[];
+  pointPerPerson: string | null;
+  tags: string[] | null;
   title: string;
-  upload: {
-    lastModified: number;
-    lastModifiedDate?: Date;
-    name: string;
-    originFileObj: File;
-    percent: number;
-    size: number;
-    type: string;
-    uid: string;
-  }[];
 };
 
 const TopicCreate = () => {
-  const valuesRef = useRef<FormDataType | null>(null);
-
-  const onSuccessPostTopic = () => {
+  const navigate = useNavigate();
+  const onSuccessPostTopic = (res: number) => {
     alert("토픽이 생성되었습니다");
+    navigate(`/admin/topic/edit/${encrypt(res)}`);
   };
 
   const { mutate: topicCreate, isLoading: topicCreateIsLoading } =
@@ -37,18 +29,30 @@ const TopicCreate = () => {
 
   const isLoading = topicCreateIsLoading;
 
-  const topicSubmit = (values: FormDataType) => {
-    valuesRef.current = values;
-    const tagString = values.tags.join();
+  const topicSubmit = (data: FormDataType) => {
     const topicCreateData = {
-      topicTitle: values.title,
-      topicDesc: values.description,
-      topicNotice: values.notice,
-      topicTags: tagString,
-      topicPoint: values.pointPerPerson,
+      topicTitle: data.title,
+      topicDesc: data.description,
+      topicNotice: data.notice,
+      topicTags: data?.tags?.join(",") || "",
+      topicPoint: data.pointPerPerson || "",
     };
     topicCreate(topicCreateData);
   };
+
+  const defaultValues: FormDataType = {
+    title: "",
+    description: "",
+    notice: "",
+    tags: null,
+    pointPerPerson: null,
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataType>({ defaultValues });
 
   return (
     <>
@@ -57,129 +61,70 @@ const TopicCreate = () => {
       ) : (
         <>
           <AdminFormLayOut title={"토픽 생성 페이지"}>
-            <Form onFinish={topicSubmit} className="w-full max-w-[1200px]">
-              <FormTitle />
-              <FormDesc />
-              <FormInterest />
-              <FormPoint />
-              <SubmitButtom />
-            </Form>
+            <form
+              onSubmit={handleSubmit(topicSubmit)}
+              className="w-full flex flex-col gap-6"
+            >
+              <Input
+                id="title"
+                label="토픽 제목"
+                registration={register("title", {
+                  required: "제목을 입력해주세요",
+                })}
+                error={errors.title}
+                required
+              />
+              <TextArea
+                id="description"
+                label="간단한 소개"
+                registration={register("description", {
+                  required: "간단 소개를 입력해주세요",
+                })}
+                error={errors.description}
+                required
+                rows={4}
+              />
+              <TextArea
+                id="notice"
+                label="유의사항"
+                registration={register("notice", {
+                  required: "유의사항을 입력해주세요",
+                })}
+                error={errors.notice}
+                required
+                rows={4}
+              />
+
+              <Select
+                id="tags"
+                label="관심사 선택"
+                options={interestsOption}
+                registration={register("tags", {
+                  required: "관심사 태그를 설정해주세요",
+                })}
+                error={errors.tags}
+                required
+                multiple
+              />
+
+              <Input
+                id="pointPerPerson"
+                label="포인트"
+                registration={register("pointPerPerson", {
+                  required: "포인트를 입력해주세요",
+                })}
+                error={errors.pointPerPerson}
+                required
+              />
+              <div className="flex justify-center gap-32">
+                <button className="rounded-xl w-[10rem] h-[5rem] text-white bg-_neutral-70 text-_h3 hover:opacity-65">
+                  생성
+                </button>
+              </div>
+            </form>
           </AdminFormLayOut>
         </>
       )}
-    </>
-  );
-};
-
-const FormTitle = () => {
-  return (
-    <>
-      <Form.Item
-        label="토픽 제목"
-        rules={[
-          {
-            validator: async (_, names) => {
-              if (!names || names.length < 1) {
-                return Promise.reject(new Error("제목을 입력해주세요"));
-              }
-            },
-          },
-        ]}
-        name="title"
-      >
-        <Input />
-      </Form.Item>
-    </>
-  );
-};
-const FormDesc = () => {
-  const [simpleInfo, setSimpleInfo] = useState("");
-  const [notice, setTopicNotice] = useState("");
-
-  const simpleInfoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setSimpleInfo(e.target.value);
-  };
-
-  const noticeChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setTopicNotice(e.target.value);
-  };
-
-  return (
-    <>
-      <Form.Item
-        label="간단한 소개"
-        validateStatus={simpleInfo ? "success" : "error"}
-        hasFeedback
-        help={simpleInfo ? null : "간단한 소개를 입력해주세요"}
-        name="description"
-      >
-        <Input.TextArea allowClear showCount onChange={simpleInfoChange} />
-      </Form.Item>
-      <Form.Item
-        label="유의사항"
-        validateStatus={notice ? "success" : "error"}
-        hasFeedback
-        help={notice ? null : "유의사항을 입력해주세요"}
-        name="notice"
-      >
-        <Input.TextArea allowClear showCount onChange={noticeChange} />
-      </Form.Item>
-    </>
-  );
-};
-
-const FormInterest = () => {
-  return (
-    <>
-      <Form.Item name="tags" label="관심사 선택">
-        <Select
-          mode="multiple"
-          placeholder="챌린지에 해당되는 관심사를 선택하세요"
-        >
-          {interestsOption.map((option) => (
-            <Select.Option key={option.value} value={option.value}>
-              {option.label}
-            </Select.Option>
-          ))}
-        </Select>
-      </Form.Item>
-    </>
-  );
-};
-
-const FormPoint = () => {
-  return (
-    <>
-      <Form.Item
-        label="포인트"
-        rules={[
-          {
-            validator: async (_, names) => {
-              if (!names || names.length < 1) {
-                return Promise.reject(new Error("인당 포인트를 입력해주세요"));
-              }
-            },
-          },
-        ]}
-        name="pointPerPerson"
-      >
-        <Input />
-      </Form.Item>
-    </>
-  );
-};
-
-const SubmitButtom = () => {
-  return (
-    <>
-      <div className="flex justify-center gap-32">
-        <Button
-          htmlType="submit"
-          className="w-[10rem] h-[5rem] text-white bg-_neutral-70 text-_h3 hover:opacity-65"
-        >
-          생성
-        </Button>
-      </div>
     </>
   );
 };
